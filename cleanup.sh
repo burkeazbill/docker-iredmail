e129e6579fef775527b5b87d7f69b7b5cf302b62
Burke Azbill <burkeazbill@gmail.com>
2018-03-23 16:49:32 -0400

    fixed typo

--- cleanup.sh
+++ cleanup.sh
@@ -4,7 +4,7 @@ docker-compose rm -f
 docker rmi iredmail
 # Now reset files back to original and remove *.bak
 cat iredmail.bak > iredmail.cfg
-at docker-compose.bak > docker-compose.yml
+cat docker-compose.bak > docker-compose.yml
 cat iredmail/iredmail.bak iredmail/iredmail.sh
 find ./ -name "*.bak" -type f -delete
 # Uncomment following line if you want the data in docker host volume
